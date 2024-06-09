06e4c9e28e6c373ed5b7abffbe1f1cc61647bc5c
kovalchykigor <kovalchykigor@gmail.com>
2024-06-09 18:51:36 +0300

    Errors are corrected

--- lesson_06/homework_61.py
+++ lesson_06/homework_61.py
@@ -2,4 +2,9 @@
 user_chars = input("Please input random sequence of chars:\n")
 unic_chars = set(user_chars)
 
-print(len(unic_chars))
+count_of_chars = len(unic_chars)
+print('Count of chars =', count_of_chars)
+if count_of_chars > 10:
+    print(True)
+else:
+    print(False)
