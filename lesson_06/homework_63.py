f5cbfe230afd223bd0df768b43412df8b0832ae2
kovalchykigor <kovalchykigor@gmail.com>
2024-06-11 13:55:14 +0300

    homework_63 deleted

--- lesson_06/homework_63.py
+++
@@ -1,6 +0,0 @@
-lst1, lst2 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'], []
-for element in lst1:
-    if isinstance(element, str):
-        lst2.append(element)
-
-print(lst2)
